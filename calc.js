d01312bdfb5fc78a271c550d34085715e73b79d2
16sheep <marjutubli@gmail.com>
2017-01-28 00:08:13 +0000

    FCC JS Calculator First Draft

---
+++ calc.js
@@ -0,0 +1,49 @@
+var numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+var opArr = ["*", "/", "-", "+"]
+var finalRes = ""
+$("button").click(function () {
+  var curId = this.id
+  var curHTML = getById(curId).innerHTML
+  if (curId == "ac") {
+    getById("result").innerHTML = ""
+    getById("input").innerHTML = ""
+    finalRes = 0
+  } else if (curId == "ce") {
+    finalRes = finalRes.slice(0, -1)
+    getById("result").innerHTML = finalRes.charAt(finalRes.length - 1)
+    getById("input").innerHTML = finalRes
+  } else if (numArr.indexOf(parseInt(curHTML)) > -1) {
+    if (getById("result").innerHTML.charAt(0) == "-") {
+      getById("result").innerHTML += curHTML
+      getById("input").innerHTML += curHTML + ")"
+      finalRes += curHTML + ")"
+    } else {
+      getById("result").innerHTML += curHTML
+      getById("input").innerHTML += curHTML
+      finalRes += curHTML
+    }
+  } else if (opArr.indexOf(getById(curId).innerHTML) > -1) {
+    getById("result").innerHTML = curHTML
+    getById("input").innerHTML += curHTML
+    finalRes += curHTML
+  } else if (curId == "dot") {
+    getById("result").innerHTML += curHTML
+    getById("input").innerHTML += curHTML
+    finalRes += curHTML
+  } else if (curId == "neg") {
+    getById("result").innerHTML = "-"
+    getById("input").innerHTML += "(-"
+    finalRes += "(-"
+  } else if (curId == "equals") {
+    getById("result").innerHTML = Math.round(eval(finalRes) * 100000000) / 100000000
+  }
+
+});
+
+function getById(element) {
+  return document.getElementById(element)
+}
+
+function getByClass(element) {
+  return document.getElementsByClassName(element)
+}
