ade857bf3d07d150f7554d56a0bd1df80d8cf49a
16sheep <marjutubli@gmail.com>
2017-01-29 21:59:41 +0000

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,9 +1,11 @@
 var numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 var opArr = ["*", "/", "-", "+"]
 var finalRes = ""
+
 $("button").click(function () {
   var curId = this.id
   var curHTML = getById(curId).innerHTML
+  var resHTML = getById("result").innerHTML
   if (curId == "ac") {
     getById("result").innerHTML = ""
     getById("input").innerHTML = ""
@@ -13,21 +15,26 @@ $("button").click(function () {
     getById("result").innerHTML = finalRes.charAt(finalRes.length - 1)
     getById("input").innerHTML = finalRes
   } else if (numArr.indexOf(parseInt(curHTML)) > -1) {
-    if (getById("result").innerHTML.charAt(0) == "-") {
-      getById("result").innerHTML += curHTML
-      getById("input").innerHTML += curHTML + ")"
-      finalRes += curHTML + ")"
-      console.log(finalRes)
-    } else {
-      getById("result").innerHTML += curHTML
-      getById("input").innerHTML += curHTML
-      finalRes += curHTML
-      console.log(finalRes)
-    }
-  } else if (opArr.indexOf(getById(curId).innerHTML) > -1) {
+    getById("result").innerHTML += curHTML
+    getById("input").innerHTML += curHTML
+    finalRes += curHTML
+    console.log(finalRes)
+
+  } else if (opArr.indexOf(curHTML) > -1) {
+    var parMatch = finalRes.match(/[(]/g)
+
     getById("result").innerHTML = curHTML
     getById("input").innerHTML += curHTML
     finalRes += curHTML
+
+    if (parMatch.length == true && parMatch.length % 2 != 0) {
+      getById("result").innerHTML = curHTML
+      getById("input").innerHTML = getById("input").innerHTML.slice(0, getById("input").innerHTML.length - 1)
+      getById("input").innerHTML += ")" + curHTML
+      finalRes.slice(0, finalRes.length - 1)
+      finalRes = finalRes += ")" + curHTML
+    }
+
   } else if (curId == "dot") {
     getById("result").innerHTML += curHTML
     getById("input").innerHTML += curHTML
